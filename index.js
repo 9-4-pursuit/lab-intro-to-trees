4c038725ffa0c950ba9acac6819692742341c315
BertCasale <bthebest55@gmail.com>
2023-07-09 14:56:43 -0400

    findNode works for internal and leaf nodes

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ class Tree {
   findNode(data) {
     if (this.root.data === data) return this.root;
 
+    let queue = [this.root];
+    while (queue.length > 0) {
+        let currentNode = queue.shift();
+        if (currentNode === null) continue;
+
+        if(currentNode.data === data) return currentNode;
+
+        queue.push(...currentNode.children);
+    }
+
     return null;
   }
 }
