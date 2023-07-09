8962dd408602986f9a1ba2c0fc7ef272f646facc
ninajavier <ninajavier13@gmail.com>
2023-07-09 15:18:31 -0400

    Can properly search for a node

--- index.js
+++ index.js
@@ -10,7 +10,21 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    let queue = [this.root];
+
+    while (queue.length > 0) {
+      let currentNode = queue.shift();
+      if (currentNode === null) continue;
+
+      if (currentNode.data === data) {
+        return currentNode;
+      }
+
+      queue.push(...currentNode.children);
+    }
+    return null;
+  }
 }
 
 module.exports = { TreeNode, Tree };
