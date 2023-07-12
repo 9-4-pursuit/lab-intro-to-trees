b3387bcd48c929078e734d5014405d982091b256
Felizj17 <felizj17@gmail.com>
2023-07-12 13:37:01 -0400

    lab complete use of BFS

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 class TreeNode {
   constructor(data = null) {
-    this.data = data;
-    this.children = [];
+    this.data = data
+    this.children = []
   }
 }
 
 class Tree {
   constructor(node) {
-    this.root = node;
+    this.root = node
   }
 
-  findNode(data) {}
+  findNode(data) {
+    let queue = [this.root]
+
+    while (queue.length > 0) {
+      let currentNode = queue.shift()
+      if (currentNode === undefined) continue
+
+      if (currentNode.data === data) {
+        console.log('currentNode:', currentNode, 'data:', data)
+        return currentNode
+      }
+
+      queue.push(...currentNode.children)
+      // queue.push(currentNode.children[0], currentNode.children[1])
+    }
+    return null
+  }
 }
 
-module.exports = { TreeNode, Tree };
+module.exports = {TreeNode, Tree}
