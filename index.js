0ef3b43e8797f9f42dd361ac9c381a69d7c0c06b
MichelBDLC <michelbdlc@gmail.com>
2023-07-25 00:04:55 -0700

    found node, first 2 tests passing

--- index.js
+++ index.js
@@ -10,7 +10,36 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+
+    let node = [this.root];
+
+    while (node.length > 0) {
+      let currentNode = node.shift();
+      if (currentNode.data === data) {
+        return currentNode;
+
+      } 
+
+      
+    }
+
+
+    // while ([this.root]) {
+    //   if ([this.root].data === data) {
+    //     break;
+    //   }
+    //   parent = [this.root];
+    //   [this.root] = (data > [this.root].data) ? [this.root].right : [this.root].left
+    // }
+
+    // return { found: [this.root], parent}
+  
+
+    
+
+
+  }
 }
 
 module.exports = { TreeNode, Tree };
